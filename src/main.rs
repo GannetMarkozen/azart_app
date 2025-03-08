edcb555581baa66e77adfcfcd7af069f94d0e5dc
Gannet Markozen <gannetmarkozen@gmail.com>
2025-03-07 17:30:44 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod azart;
+use azart::*;
+
+use bevy::prelude::*;
+
+fn main() {
+	#[cfg(debug_assertions)]
+	{
+		//unsafe { std::env::set_var("RUST_BACKTRACE", "full"); }
+		color_eyre::install().unwrap();
+	}
+
+	App::new()
+		.add_plugins(AzartPlugin)
+		.run();
+}
